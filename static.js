c7cf8527c983041c4469023e2c7cf7bcb3d2d0c5
Cynthia Hutauruk <cyn.uruk@gmail.com>
2022-10-19 23:37:50 -0600

    corrected super constructor, lets, and functions vs. objects

--- static.js
+++ static.js
@@ -17,45 +17,64 @@ class Person {
         this.legalAge = legalAge;
         this.county = county;
     }
-    vote(name1)
+    vote(name1){
         console.log(name1, "will be voting for himself in this year's election.")
-    
-    reside(county)
-        console.log(county, "belongs to Colorado.")
-
-    drinks(legalAge)
-        console.log(legalAge, "can drink with other adults.")
+    }
+    reside(county){
+        console.log(county, "lives in " + this.county + " county.")
+    }
+    drinks(legalAge){
+        console.log(legalAge, "can drink with other adults since they are over " + this.legalAge + ".")
+    }
 }
-
-Person.vote('Nhan')
-Person.reside('Will')
-Person.drinks('Ying')
+let myPerson = new Person('Unaffiliated', 21, 'Arapahoe');
+console.log(myPerson);
+myPerson.vote('Nhan');
+myPerson.reside('Nhan');
+myPerson.drinks('Nhan');
 
 class Postal extends Person {
-    constructor(lost, customer){
+    constructor(party, legalAge, county, lost, customer){
+        super(party, legalAge, county);
         this.lost = lost;
         this.customer = customer;
     }
-    package(lost)
-        console.log(lost, " did not deliver all your mail to your address.")
-    
-    interface(customer)
-        console.log(customer, " placed a stamp on the customer's package.")
+    package(lost){
+        console.log(lost, "did not deliver all expected mail to my address.")
+    }
+    interface(customer){
+        console.log(customer, "placed a stamp on the customer's package.")
+    }
 }
+let myPostal1 = new Postal('Conservative', 22, 'Broomfield');
+console.log(myPostal1);
+myPostal1.package('Will');
+myPostal1.interface('Will');
 
-Postal.lost('Dulce')
-Postal.interface('Vandana')
+const myPostal2 = new Postal('Progressive', 31, 'Broomfield');
+console.log(myPostal2);
+myPostal2.package('Ying');
+myPostal2.interface('Ying');
 
 class Chef extends Person {
-    constructor(shop, sharpen){
+    constructor(party, legalAge, county, shop, sharpen){
+        super(party, legalAge, county);
         this.shop = shop;
         this.sharpen = sharpen;
     }
-    apron(shop)
-        console.log(shop " bought a new apron at the store.")
-    knives(sharpen)
-        console.log(sharpen " made three knives sharper.")
+    apron(shop){
+        console.log(shop, "bought a new apron at the store.")
+    }
+    knives(sharpen){
+        console.log(sharpen, "made three knives sharper.")
+    }
 }
+let myChef1 = new Chef('Republican', 35, 'Denver');
+console.log(myChef1);
+myChef1.apron('Jason');
+myChef1.knives('Jason');
 
-Chef.shop('Jason')
-Chef.interface('Jason')
+let myChef2 = new Chef('Libertarian', 26, 'Douglas');
+console.log(myChef2);
+myChef2.apron('Dulce');
+myChef2.knives('Dulce');
